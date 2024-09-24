4b95217bd44063693105d8144a6df760fafc6cbc
Anuj upadhyay <153272046+AnujUpadhyay18@users.noreply.github.com>
2024-09-24 11:15:01 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const express= require('express')
+const cors=require('cors')
+const app= express();
+const dotenv=require('dotenv')
+dotenv.config();
+const locationData= require('./location.json')
+app.use(cors())
+
+
+app.get('/api/location',(req,res)=>{
+       const data = locationData;
+        res.json(data);
+      });
+
+app.listen(process.env.PORT||3000);
